207acf2c01b27d1da7315cc6f1773c68e7e6c99d
Camille Scott <cswel@ucdavis.edu>
2021-02-16 01:40:40 -0800

    Bump version: 0.2.1 → 0.3.0

--- zardoz/__init__.py
+++ zardoz/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Camille Scott"""
 __email__ = 'cswel@ucdavis.edu'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __splash__ = f'''
  _____             _      _____
 |__  /__ _ _ __ __| | ___|__  /
