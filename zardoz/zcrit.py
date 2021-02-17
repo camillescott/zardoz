b0c92fa2ad6ee0e78fb2beb9a619e1243f8464fd
Camille Scott <cswel@ucdavis.edu>
2021-02-17 14:33:46 -0800

    Add list subcommand for zcrit

--- zardoz/zcrit.py
+++ zardoz/zcrit.py
@@ -10,52 +10,6 @@ from discord.ext import commands
 from .logging import LoggingMixin
 from .utils import __pkg_dir__
 
-class CritCommands(commands.Cog, LoggingMixin):
-
-    def __init__(self, bot, db):
-        self.bot = bot
-        self.db = db
-        super().__init__()
-
-        self.tables = load_crit_tables(self.log)
-        self.log.info(f'Loaded crits: {self.tables}')
-
-    @commands.group(name='zcrit', help='Roll on a crit table.')
-    async def zcrit(self, ctx):
-        pass
-
-    @zcrit.command(name='r')
-    async def zcrit_roll(self, ctx, table_name: str, val: typing.Optional[int]):
-        try:
-            table = self.tables[table_name]
-        except KeyError:
-            self.log.error(f'Error: no such crit: {table_name}.')
-            await ctx.message.reply(f'No crit table named {table_name}.')
-        else:
-            try:
-                if val is None:
-                    val, name, effect = table.roll()
-                else:
-                    name, effect = table.get(val)
-            except ValueError:
-                await ctx.message.reply(f'Bad crit table value. Perils be upon ye.')
-            else:
-                msg = f'**Result:** {table.die} ⤳ {val}\n'\
-                      f'**Table:** {table.full_name} ({table.game}, {table.book})\n'\
-                      f'**Name:** {name}\n'\
-                      f'**Effect:** {effect}'
-                await ctx.message.reply(msg)
-
-
-def load_crit_tables(log=None):
-    
-    files = glob.glob(os.path.join(__pkg_dir__, 'crits', '*.yaml'))
-    tables = {}
-    for file_path in files:
-        table = CritTable(file_path)
-        tables[table.slug] = table
-    return tables
-
 
 class CritTable:
 
@@ -87,3 +41,67 @@ class CritTable:
         return rolled_val, name, effect
 
 
+def load_crit_tables(log=None):
+    
+    files = glob.glob(os.path.join(__pkg_dir__, 'crits', '*.yaml'))
+    tables = {}
+    for file_path in files:
+        table = CritTable(file_path)
+        tables[table.slug] = table
+    return tables
+
+
+TABLES = load_crit_tables()
+
+
+class TableConvert(commands.Converter):
+
+    async def convert(self, ctx, argument):
+        try:
+            table = TABLES[argument]
+        except KeyError:
+            raise commands.BadArgument(f'{argument} is not a valid table.')
+        return table
+
+
+class CritCommands(commands.Cog, LoggingMixin):
+
+    def __init__(self, bot, db):
+        self.bot = bot
+        self.db = db
+        super().__init__()
+
+        self.log.info(f'Loaded crits: {TABLES}')
+
+    @commands.group(name='zcrit', help='Roll on a crit table.')
+    async def zcrit(self, ctx, table: typing.Optional[TableConvert],
+                               val: typing.Optional[int]):
+        if ctx.invoked_subcommand is not None:
+            return
+
+        if table is None:
+            await ctx.invoke(self.zcrit_list)
+            return
+
+        try:
+            if val is None:
+                val, name, effect = table.roll()
+            else:
+                name, effect = table.get(val)
+        except ValueError:
+            await ctx.message.reply(f'Bad crit table value. Perils be upon ye.')
+        else:
+            msg = f'**Result:** {table.die} ⤳ {val}\n'\
+                  f'**Table:** {table.full_name} ({table.game}, {table.book})\n'\
+                  f'**Name:** {name}\n'\
+                  f'**Effect:** {effect}'
+            await ctx.message.reply(msg)
+
+    @zcrit.command(name='list', help='List available crit tables.')
+    async def zcrit_list(self, ctx):
+        header = '**Available Tables:**'
+        body = []
+        for slug, table in TABLES.items():
+            body.append(f'`{slug:15}` {table.full_name} ({table.game}, {table.book})')
+        body = '\n'.join(body)
+        await ctx.message.reply(f'{header}\n{body}')
