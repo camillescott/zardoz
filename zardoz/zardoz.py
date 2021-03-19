72ec9d64384ead2deb18a966b5fa631a839132c5
Camille Scott <camille.scott.w@gmail.com>
2021-03-19 13:05:01 -0700

    Link to releases/latest from zabout

--- zardoz/zardoz.py
+++ zardoz/zardoz.py
@@ -242,7 +242,7 @@ def dize(args):
     @bot.command(name='zabout', help='Project info.')
     async def zabout(ctx):
         msg = f'version: {__version__}\n'\
-              f'source: https://github.com/camillescott/zardoz\n'\
+              f'source: https://github.com/camillescott/zardoz/releases/latest\n'\
               f'active installs: {len(bot.guilds)}'
         await ctx.message.reply(msg)
 
