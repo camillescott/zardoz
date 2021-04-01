50c7abffbc317cd9d1fbd9b578062c3763b8101a
Camille Scott <camille.scott.w@gmail.com>
2021-04-01 14:05:55 -0700

    Update dependencies

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = ['discord.py',
+                'disputils',
                 'rich',
                 'dice',
                 'pyyaml',
